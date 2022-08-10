59890109c2eddbf300b37cab580304bf1070bbd4
Sahrul Maulana <110948971+Sahrul-Maulana@users.noreply.github.com>
2022-08-10 13:46:58 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,49 +2,6 @@
 
 set -eu
 
-msg() {
-  echo -e "\e[1;32m$*\e[0m"
-}
+export GITHUB="true"
 
-figlet Welcome
-msg =============================================
-msg =•••••••••••••••••Powered By••••••••••••••••=
-msg =••••••••••••••••NFS-project™•••••••••••••••=
-msg =============================================
-
-SSHPATH="$HOME/.ssh"
-
-if [ ! -d "$SSHPATH" ]
-then
-  mkdir "$SSHPATH"
-fi
-
-if [ ! -f "$SSHPATH/known_hosts" ]
-then
-  touch "$SSHPATH/known_hosts"
-fi
-
-echo "$INPUT_KEY" > "$SSHPATH/deploy_key"
-chmod 700 "$SSHPATH"
-chmod 600 "$SSHPATH/known_hosts"
-chmod 600 "$SSHPATH/deploy_key"
-
-echo "$INPUT_COMMAND" > $HOME/shell.sh
-echo "exit \$?" >> $HOME/shell.sh
-cat $HOME/shell.sh
-
-echo 💥 Connecting to SSH 💥
-
-if [ "$INPUT_PASS" = "" ]
-then
-  sh -c "ssh $INPUT_ARGS -i $SSHPATH/deploy_key -o StrictHostKeyChecking=no -p $INPUT_PORT ${INPUT_USER}@${INPUT_HOST} < $HOME/shell.sh"
-else
-  sh -c "sshpass -p $INPUT_PASS ssh $INPUT_ARGS -o StrictHostKeyChecking=no -p $INPUT_PORT ${INPUT_USER}@${INPUT_HOST} < $HOME/shell.sh"
-fi
-
-echo SSH execution Done ✅
-figlet God Bay
-msg =============================================
-msg =•••••••••••••••••Powered By••••••••••••••••=
-msg =••••••••••••••••NFS-project™•••••••••••••••=
-msg =============================================
+sh -c "/bin/drone-ssh $*"
